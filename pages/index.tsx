40dca01e15f155bf793f31d8ae9b57c9c2aa7c20
Jeong-minji <alswl4152@naver.com>
2022-01-04 14:31:18 +0900

    Add: 프로젝트 초기 세팅 완료

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import styled from "styled-components";
+
+const Index: NextPage = () => {
+  return <div>hi</div>;
+};
+
+export default Index;
---
+++ styles/GlobalStyle.ts
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+export const GlobalStyle = createGlobalStyle`
+  ${reset}
+  
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  @font-face {
+    font-family: AmaticSC;
+    src: url('/fonts/AmaticSC-Regular.ttf') format('truetype');
+  }
+  @font-face {
+    font-family: AmaticSCB;
+    src: url('/fonts/AmaticSC-Bold.ttf') format('truetype');
+  }
+  @font-face {
+    font-family: Hangang;
+    src: url('/fonts/SEOULHANGANGB.TTF') format('truetype');
+  }
+  @font-face {
+    font-family: HangangB;
+    src: url('/fonts/SEOULHANGANGEB.TTF') format('truetype');
+  }
+`;
